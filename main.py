0d146d9fc7bc0e28c4b1242bf56cb5fe3239e1c1
lililitabibit0-cpu <lililitabibit0@gmail.com>
2026-05-10 16:19:09 +0300

    asdfg

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from random_letters import generat_btn
 # Імпортувати генератор випадкових чисел
 from random import randint
 '''Імпортувати класи гравця і ворога'''
-
+from player import Player, Enemy
 '''Імпортувати Меню з файлу меню'''
-
+from menu import Menu
 # Запустити ініціалізацію pygame
 pygame.init()
 
@@ -33,10 +33,14 @@ def restart():
     game_part = "game"
     '''5.виправити генерацію букв за рівнем гри
     зі списка LEVELS беремо рівень номер що зберігається в меню menu_ui.level'''
-   
+    btns = generat_btn(LEVELS[menu_ui.level])
     '''5.як фон увімкнений(перервіряємо прапор з меню fon_music)
     завантажити фонову музику зі списку FON_MUSICS за нмоером menu_ui.music
     налашутвати гучність - 0.5, включити на програвання -1'''
+    if menu_ui.fon_music:
+        pygame.mixer.music.load(FON_MUSICS[menu_ui.music])
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)
     
     player = Player(100, HEIGHT//2+75, 50, 50, 5, PLAYERS)
     enemy = Enemy(10, HEIGHT//2+75, 50, 50, 3, ENEMYS)
@@ -44,10 +48,15 @@ def restart():
 '''4.Створити функцію обробник для зміни на вікно меню
 міняємо зміну game_part'''
 def open_menu():
+    global game_part
+    game_part = "menu"
+
     ...
 '''4.Створити функцію обробник для зміни на вікно старт
 міняємо зміну game_part'''
 def back():
+    global game_part
+    game_part = "start"
     ...
 # Створити головне вікно гри з заданими розмірами
 # Встановити назву вікна
@@ -69,24 +78,33 @@ start_btn = Button(200,200,200,60,BLUE,
 х,у = 200, 300, ширина,висота - 200 60
 кольри -BLUE,  WHITE, текст - Menu
 команда - open_menu'''
+menu_bt = Button(200,300,200,60,BLUE,
+                     "Menu",WHITE,command = open_menu)
 
 '''4.Створити меню
-координати - 200 50, розмір 200б 85'''
-
+координати - 200 50, розмір 200, 85'''
+menu_ui = Menu(200,50,200,85)
 '''4.Створити кнопку для переходу назад у старт з гри
 х,у = 20, 20, ширина,висота - 100 30
 кольри -BLUE,  WHITE, текст - Back
 команда - back'''
-
+start_returt = Button(20,20,100,30,BLUE,
+                     "Back",WHITE,command = back)
 # Створити кнопку кінця гри
 # Створити кнопку перезапуску гри
 btn_end = Button(200,150,200, 80,RED,
                    "EXIT",WHITE, command = quit)
 btn_restart = Button(200,270,200, 100,BLUE,
                    "RESTART",WHITE, command = restart)
+btn_menu = Button(200,270,200, 100,BLUE,
+                   "MENU",WHITE, command = open_menu)
+
+
 
 '''1.створити глобальні змінні для гравця, ворога, та кнопок'''
-
+player=None
+enemy=None
+btns=[]
 # Увімкнути головний цикл гри
 # Встановити початковий стан меню
 # Змінна, яка визначає перемогу або поразку
@@ -106,13 +124,17 @@ while run :
         if event.type == pygame.QUIT:
             run = False
         '''1.Перевірити чи натсинута клавіша , триває гра та ще є букви-кнопки'''
-        
+        if event.type == pygame.KEYDOWN and game_part == "game" and btns:
             '''1.отримати назву кнокпи натиснутої - event.unicode'''
-            
+            btn_name = event.unicode
             '''1. Перевірити назву кнокпи та кнопки букви-btns[0].letter'''
-            
+            if btn_name == btns[0].letter:
                 '''1.якщо вірна видалити її зі списку btns'''
-               
+                del btns[0]
+          
+                
+
+
 
 # Якщо гра в меню
 # Намалювати кнопку старту
@@ -120,6 +142,8 @@ while run :
     if game_part== "start":
         start_btn.draw(window)
         start_btn.is_clicked()
+        menu_bt.draw(window)
+        menu_bt.is_clicked()
         '''6.відобразити та перевірити клік по кнопці меню'''
        
         
@@ -144,9 +168,14 @@ while run :
         enemy.update(window)
         '''2. Перевірка на вигращ - якщо кнопок-букв не залишилось
         змінити game_part та end_game'''
-
+        if len(btns) == 0:
+            end_game = "win"
+            game_part = "end"
         '''2. Перевірка на програш - якщо ДІГНАВ ВОРОГО
         змінити game_part та end_game'''
+        if player.rect.colliderect(enemy.rect):
+            end_game = "lose"
+            game_part = "end"
         
 
 
@@ -171,7 +200,11 @@ while run :
     '''6.яКЩО ЗАРАЗ МЕНЮ: онвоити меню
     якщо прапор в меню menu_ui.back_start- 
     змінити змінну game_part та прапор menu_ui.back_start'''
-   
+    if game_part == "menu":
+        menu_ui.update(window)
+        if menu_ui.back_start:
+            game_part = "start"
+            menu_ui.back_start = False
 
 # Оновити екран (показати всі зміни)
 # Обмежити FPS гри
--- menu.py
+++ menu.py
@@ -4,36 +4,62 @@ from settings import*
 '''3. Створи лкас меню'''
 class Menu:
     '''Консруткор приймає координати та розмір'''
-    def 
+    def __init__(self,x,y,w,h):
         '''створити змінні -властивості:
         music - номер музики
         fon_music - вкл-викл музики
         level  - номер рівня 
         back_start - прапор назад до старту-вікна
         '''
-       
+        self.music = 0
+        self.fon_music= False
+        self.level = 0
+        self.back_start = False
         '''Створити 4 кнопки:
-        координати: х = хб у = у,y+h+15,y+2*h+30, y+3*h+45
+        координати: х = х, у = у,y+h+15,y+2*h+30, y+3*h+45
         розмір - w, h, колір - BLUE,GREY,
         тексти - Fon music: off,Music: off, Level: 1,  Back
         команди - відповідні   '''
-        self.btn_music 
-        self.btn_on_of 
-        self.btn_level 
-        self.btn_back 
+        self.btn_music = Button( x,y,w,h,GREY,"Music: off",BLUE,command=self.chose_music)  
+        self.btn_on_of = Button( x,y+h+15,w,h,GREY,"Fon music: off",BLUE,command=self.on_off_fon)
+        self.btn_level = Button( x,y+2*h+30,w,h,GREY,"Level: 1",BLUE,command=self.chose_level)
+        self.btn_back  = Button( x,y+3*h+45,w,h,GREY,"Back",BLUE,command=self.back)
+
     
 
     '''Метод оновлення меню - малюємо кнопки , виклик кліка по кнопках'''
     def update(self, window):
+        self.btn_music.draw(window)
+        self.btn_on_of.draw(window)
+        self.btn_level.draw(window)
+        self.btn_back.draw(window)
+        self.btn_music.is_clicked()
+        self.btn_on_of.is_clicked()
+        self.btn_level.is_clicked()
+        self.btn_back.is_clicked()
       
     '''Метод вибору пісня'''
     def chose_music(self):
+        self.music += 1
+        if self.music == len(FON_MUSICS):
+            self.music = 1
+        self.btn_music.add_text( f"Music: {FON_MUSICS[self.music]}")
         
     '''Метод вклю викл музики'''    
     def on_off_fon(self):
+        if self.fon_music:
+            self.btn_on_of.add_text ( "Fon music: on")
+            self.fon_music = False
+        else:
+            self.btn_on_of.add_text ( "Fon music: off")
+            self.fon_music = True
         
     '''Метод вибору рівня'''
     def chose_level(self):
+        self.level += 1
+        if self.level == len(LEVELS):
+            self.level = 0
+        self.btn_level.add_text(f"Level: {LEVELS[self.level]}")
     
     '''Метод повренутись назад'''
     def back(self):
